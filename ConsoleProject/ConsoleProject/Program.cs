af75c933000359241ffab5589dab2c7532122c0f
Ibai Osa <iosa@kirogrifols.com>
2017-10-23 12:25:56 +0200

    Beñat escrito mejor

--- ConsoleProject/ConsoleProject/Program.cs
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleProject
         {
             for (int i = 10; i > 0; i--)
             {
-                System.Console.WriteLine("Beñat y Ibai"+i);
+                System.Console.WriteLine("Bñat y Ibai"+i);
             }
             System.Console.WriteLine("Pulse cualquier botón para finalizar");
             System.Console.ReadLine();
