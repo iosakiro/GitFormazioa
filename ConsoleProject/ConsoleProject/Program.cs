648e76da92885698fb983af4a1904d0a0629d9ae
Ibai Osa <iosa@kirogrifols.com>
2017-10-23 08:58:09 +0200

    First update

---
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(i);
+                System.Threading.Thread.Sleep(1000);
+            }
+            System.Console.WriteLine("Pulse enter para finalizar");
+            System.Console.ReadLine();
+        }
+    }
+}
